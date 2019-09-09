5c9113fbd0061eab8c2bcac198ccbbba57c23bcc
Elien Vissers <elien.visserssimilon@gmail.com>
2019-09-09 15:46:01 +0200

    add under construction handlebars template

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const app = express();
+const hb = require('express-handlebars');
+
+app.engine('handlebars', hb());
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+    res.render('underconstructionTemplate', {
+        layout: 'main'
+    });
+});
 
 app.listen(process.env.PORT || 8080, () => console.log('Listening!'));
