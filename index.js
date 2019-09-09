ce3dfd3219a8e7d9a66b7863fb1d61c6280aa6c7
Elien Vissers <elien.visserssimilon@gmail.com>
2019-09-09 18:10:25 +0200

    subdomain redirecting comments

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ app.engine('handlebars', hb());
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + "/public"));
 
+app.use(function(req, res, next) {
+  var domains = req.subdomains;
+
+//   if (/* condition */) {
+//     // your code
+//     req.url = '/something' + req.url;
+//   }
+//   else {
+//     // your code
+//     return res.redirect(somewhere);
+//   }
+//
+//   next();
+// 
+// });
+
 app.get('/', (req, res) => {
     res.render('underconstructionTemplate', {
         layout: 'main'
